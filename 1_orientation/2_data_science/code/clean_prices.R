1bd1e4ab691c9465625b6184ad6bcd5c95ac54f3
Phil Chodrow <pchodrow@gmail.com>
2019-08-18 13:12:01 -0400

    set project and include description

---
+++ 1_orientation/2_data_science/code/clean_prices.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+clean_prices <- function(df){
+	clean_price <- function(x) gsub('\\$|,', '',x) %>% as.numeric()
+	cols <- colnames(df) %>% keep(~grepl('price', .x))
+	df %>% mutate_at(cols, clean_price)
+}
