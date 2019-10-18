a672479a75e08d7ad4a8598528999ac32636776a
Phil Chodrow <pchodrow@gmail.com>
2019-10-18 11:00:24 -0400

    Update preassignment_1.R

--- 2_SIP/preassignment/preassignment_1.R
+++ 2_SIP/preassignment/preassignment_1.R
@@ -1,29 +1,27 @@
-install.packages(c('tidytext', 'randomForest', 'yardstick', 'janeaustenr','knitr','leaflet')) 
+install.packages(c('tidytext', 'randomForest', 'yardstick', 'janeaustenr','knitr','leaflet', 'cld3','modelr', 'kernlab', 'ggmap')) 
 
 library(tidyverse)
 library(tidytext)
 library(yardstick)
 library(janeaustenr)
+library(kernlab)
 
-# what does this code do? 
+
+#' what does this code do? 
 austen_books() %>% 
 	unnest_tokens(word, text) %>% 
 	group_by(word) %>% 
 	summarise(n = n()) %>% 
 	arrange(desc(n))
 
-# what about this? 
+#' What about this? 
 
-library(leaflet)
-
-m <- leaflet() %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12)
-m %>% addTiles()
-
-
-
-
-
-
-
-
-
+data_frame(
+	x = runif(100, 0, 1),
+	y = runif(100, 0, 1)
+) %>% 
+	mutate(cluster = c(specc(as.matrix(.), 5))) %>% 
+	ggplot() + 
+	aes(x = x, y = y, color = factor(cluster)) + 
+	geom_point() + 
+	theme_minimal()
